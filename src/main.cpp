dd238114d5d98b14f59384ea13429896b5551bab
Kadu - Carlos Eduardo <kadu@kadu.com.br>
2020-09-01 22:58:19 -0300

    Base code to OTA, AsyncWifiManager and WebSockets

--- src/main.cpp
+++ src/main.cpp
@@ -8,15 +8,124 @@
 #include <ESPAsyncWebServer.h>
 #include <ESPAsyncWiFiManager.h>         //https://github.com/tzapu/WiFiManager
 
+#include <WebSocketsServer.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+
 //for LED status
 #include <Ticker.h>
 Ticker ticker;
 
+
+AsyncWebServer server(80);
+DNSServer dns;
+const char* PARAM_MESSAGE = "message";
+const int ws_port = 1337;
+WebSocketsServer webSocket = WebSocketsServer(1337);
+
+// Callback: receiving any WebSocket message
+void onWebSocketEvent(uint8_t client_num,
+                      WStype_t type,
+                      uint8_t * payload,
+                      size_t length) {
+
+  // Figure out the type of WebSocket event
+  switch(type) {
+
+    // Client has disconnected
+    case WStype_DISCONNECTED:
+      Serial.printf("[%u] Disconnected!\n", client_num);
+      break;
+
+    // New client has connected
+    case WStype_CONNECTED:
+      {
+        IPAddress ip = webSocket.remoteIP(client_num);
+        Serial.printf("[%u] Connection from ", client_num);
+        Serial.println(ip.toString());
+      }
+      break;
+
+    // Handle text messages from client
+    case WStype_TEXT:
+
+      // Print out raw message
+      Serial.printf("[%u] Received text: %s\n", client_num, payload);
+
+      // Toggle LED
+      if ( strcmp((char *)payload, "toggleLED") == 0 ) {
+        //led_state = led_state ? 0 : 1;
+        //Serial.printf("Toggling LED to %u\n", led_state);
+        //digitalWrite(led_pin, led_state);
+
+      // Report the state of the LED
+      } else if ( strcmp((char *)payload, "getLEDState") == 0 ) {
+        //sprintf(msg_buf, "%d", led_state);
+        //Serial.printf("Sending to [%u]: %s\n", client_num, msg_buf);
+        //webSocket.sendTXT(client_num, msg_buf);
+
+      // Message not recognized
+      } else {
+        Serial.println("[%u] Message not recognized");
+      }
+      break;
+
+    // For everything else: do nothing
+    case WStype_BIN:
+    case WStype_ERROR:
+    case WStype_FRAGMENT_TEXT_START:
+    case WStype_FRAGMENT_BIN_START:
+    case WStype_FRAGMENT:
+    case WStype_FRAGMENT_FIN:
+    default:
+      break;
+  }
+}
+
+void notFound(AsyncWebServerRequest *request) {
+    request->send(404, "text/plain", "Not found");
+}
+
+void setupWebServer() {
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+        request->send(200, "text/plain", "Hello, world");
+    });
+
+  // Send a GET request to <IP>/get?message=<message>
+  server.on("/get", HTTP_GET, [] (AsyncWebServerRequest *request) {
+      String message;
+      if (request->hasParam(PARAM_MESSAGE)) {
+          message = request->getParam(PARAM_MESSAGE)->value();
+      } else {
+          message = "No message sent";
+      }
+      request->send(200, "text/plain", "Hello, GET: " + message);
+  });
+
+  // Send a POST request to <IP>/post with a form field message set to <message>
+  server.on("/post", HTTP_POST, [](AsyncWebServerRequest *request){
+      String message;
+      if (request->hasParam(PARAM_MESSAGE, true)) {
+          message = request->getParam(PARAM_MESSAGE, true)->value();
+      } else {
+          message = "No message sent";
+      }
+      request->send(200, "text/plain", "Hello, POST: " + message);
+  });
+
+  server.onNotFound(notFound);
+
+  server.begin();
+
+  webSocket.begin();
+  webSocket.onEvent(onWebSocketEvent);
+}
+
 void tick()
 {
   //toggle state
-  int state = digitalRead(BUILTIN_LED);  // get the current state of GPIO1 pin
-  digitalWrite(BUILTIN_LED, !state);     // set pin to the opposite state
+  int state = digitalRead(LED_BUILTIN);  // get the current state of GPIO1 pin
+  digitalWrite(LED_BUILTIN, !state);     // set pin to the opposite state
 }
 
 //gets called when WiFiManager enters configuration mode
@@ -29,15 +138,12 @@ void configModeCallback (AsyncWiFiManager *myWiFiManager) {
   ticker.attach(0.2, tick);
 }
 
-AsyncWebServer server(80);
-DNSServer dns;
-
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
 
   //set led pin as output
-  pinMode(BUILTIN_LED, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
   // start ticker with 0.5 because we start in AP mode and try to connect
   ticker.attach(0.6, tick);
 
@@ -65,10 +171,46 @@ void setup() {
   Serial.println("connected...yeey :)");
   ticker.detach();
   //keep LED on
-  digitalWrite(BUILTIN_LED, LOW);
+  digitalWrite(LED_BUILTIN, LOW);
+
+
+  //OTA
+
+ 
+  // Porta padrao do ESP8266 para OTA eh 8266 - Voce pode mudar ser quiser, mas deixe indicado!
+  // ArduinoOTA.setPort(8266);
+ 
+  // O Hostname padrao eh esp8266-[ChipID], mas voce pode mudar com essa funcao
+  ArduinoOTA.setHostname("xocoronga_esp8266");
+ 
+  // Nenhuma senha eh pedida, mas voce pode dar mais seguranca pedindo uma senha pra gravar
+  // ArduinoOTA.setPassword((const char *)"123");
+ 
+  ArduinoOTA.onStart([]() {
+    Serial.println("Inicio...");
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("nFim!");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progresso: %u%%r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Erro [%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Autenticacao Falhou");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Falha no Inicio");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Falha na Conexao");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Falha na Recepcao");
+    else if (error == OTA_END_ERROR) Serial.println("Falha no Fim");
+  });
+  ArduinoOTA.begin();
+
+  // Webserver
+  setupWebServer();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  webSocket.loop();
+  ArduinoOTA.handle();
 }
